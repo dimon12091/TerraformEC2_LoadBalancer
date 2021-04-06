08bc38a488d7398f1f5b8ca12def471c583e7a1f
dimon12091 <dmytro12091@gmail.com>
2021-04-06 14:30:03 +0300

    comment main.tf

--- connection/connection(v1).ps1
+++ connection/connection(v1).ps1
@@ -1,5 +1,5 @@
 #On Windows,  running commands into a privileged shell
 Start-Service -Name Winrm
-Set–Item WSMan:\localhost\Client\TrustedHosts –Value “*”
+set-item wsman:localhost\client\trustedhosts -value * -force 
 Set-Item WSMan:\localhost\Service\EnableCompatibilityHttpListener $true
 Enter-PSSession -ComputerName “x.x.x.x” -Credential $(Get-Credential)
